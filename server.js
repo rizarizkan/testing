7463de177c8a727a997078e091a88c78f556726f
Riza <rizkan@itmi.id>
2023-01-09 07:33:14 +0700

    add

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World STAGING new');
+  res.send('Hello World STAGING OKE');
 });
 
 app.listen(PORT, HOST, () => {
