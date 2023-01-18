cf26d9d34674ceb445b1b61ecebe003d79baddb7
Riza <rizkan@itmi.id>
2023-01-18 20:35:29 +0700

    add

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World DEVEL with KUBECTL');
+  res.send('Hellow World DEVEL with KUBECTL');
 });
 
 app.listen(PORT, HOST, () => {
